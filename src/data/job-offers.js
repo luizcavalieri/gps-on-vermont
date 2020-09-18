78306b1297a7952a1f1f081956cdbb2db9ea0f94
kraken <alexpadillarosas@gmail.com>
2020-09-18 18:26:51 +1000

    removing wrong text in job offer

--- src/data/job-offers.js
+++ src/data/job-offers.js
@@ -4,7 +4,7 @@ const jobOffers = [
     title: "VR General Practitioner",
     image: "doc-patient-stethoscope.jpg",
     descriptions: [
-      "We are happy to announce a full-time GP position is available for a VR GP in August 2020 when the new Practice opens.",
+      "We are happy to announce a full-time GP position is available for a Vocationally Registered GP.",
       "Located in Vermont street Wodonga, in the heart of Wodonga's medical precinct. For further enquires please contact us"
     ],
     note: "",
