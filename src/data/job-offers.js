6b45b1ab5e1804c4627c5755c5513002253d356a
curly lemon peel <alexpadillarosas@gmail.com>
2021-06-14 15:39:20 +1000

    revamp careers page

--- src/data/job-offers.js
+++ src/data/job-offers.js
@@ -4,19 +4,22 @@ const jobOffers = [
     title: "VR General Practitioner",
     image: "doc-patient-stethoscope.jpg",
     descriptions: [
-      "We are happy to announce a full-time GP position is available for a Vocationally Registered GP.",
-      "Located in Vermont street Wodonga, in the heart of Wodonga's medical precinct. For further enquires please contact us"
+      "We are happy to announce a full-time GP position is available for a Vocationally or Non-Vocationally Registered General Practitioner.",
+      "We offer excellent rates and prestigious rooms.",
+      "You are welcome to look through the premises by appointment.",
+      "We are interested in someone willing to relocate to the area permanently.",
+      "Located in Vermont street Wodonga, in the heart of Wodonga's medical precinct. For expressions of interest and enquires, please contact us at: manager@gpsonvermont.com.au"
     ],
     note: "",
     links: [
-      { display: "Mobile: 0477 413 700", value: "0477413700" },
-      { display: "Ph: 02 6025 2189", value: "0260252189" }
+      // { display: "Mobile: 0477 413 700", value: "0477413700" },
+      // { display: "Ph: 02 6025 2189", value: "0260252189" }
     ],
     ribbonText: "Job Offer",
     ribbonTextBkgColor: "#0088ff",
     ribbonTextColor: "#f0f0f0",
-    email: "manager@gpsonvermont.com.au",
-    footer: "GPs on Vermont",
+    // email: "manager@gpsonvermont.com.au",
+    // footer: "GPs on Vermont",
     enabled: true
   }
 
--- src/views/examples/CareerPage.js
+++ src/views/examples/CareerPage.js
@@ -2,16 +2,18 @@ import React, { useEffect } from "react";
 import { headerScrolling } from "./commons";
 import OtherNavbar from "../../components/NavBars/OtherNavbar";
 import DarkFooter from "../../components/Footers/DarkFooter";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import LandingPageHeader from "../../components/Headers/LandingPageHeader";
 import { RibbonContainer, RightCornerLargeRibbon } from "react-ribbons";
 import jobOffers from "../../data/job-offers";
+import ReactLinkify from "react-linkify";
 
 const CareerPage = () => {
   useEffect(() => {
     headerScrolling();
   });
 
+  const messageTitle = "CAREER";
   const DEFAULT_IMAGE = "gpv-news.png";
 
   return (
@@ -19,11 +21,62 @@ const CareerPage = () => {
       <OtherNavbar />
       <div className="wrapper">
         <LandingPageHeader
-          backgroundImgName={"career-large.jpg"}
+          title={messageTitle}
+          backgroundImgName={"career-image.jpg"}
           imageClassName={"page-header page-header-small"}
           contentClassName={"content-center"}
           titleClassName={"title-landing-page"}
         />
+
+        <div className="section section-about-us" >
+          <ReactLinkify >
+                        
+            <Container >
+              <Row >
+                  <Col sm="12" lg="12" className="">
+                      <h5 className="description text-left">
+                      Welcome to our careers section. Here you can view our current available positions.
+                      </h5>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col sm="12" lg="12" className="">
+                      <h5 className="description text-left">
+                      GPs on Vermont Is a modern, well-equipped medical centre, conveniently located in Wodonga’s medical district. We service the broader Albury Wodonga community.
+                      </h5>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col sm="12" lg="12" className="">
+                      <h5 className="description text-left">
+                      One of the wonderful things about GPs on Vermont Medical Centre is our tight knit professional team. It has really shone this past year, so much so that the Albury Wodonga community has readily embraced us and we are able to expand and hire more staff. We are interested in finding someone who is more than their credentials. Someone who is compatible with the team, the patients and our medical centres values. As well as being willing to relocate to the area permanently.
+                      </h5>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col sm="12" lg="12" className="">
+                      <h5 className="description text-left">
+                      We are proud of being able to provide the best service and care to our patients. Whilst additionally providing a work environment where our employees are making a difference while continuing to learn, develop and thrive in their specific career interests.
+                      </h5>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col sm="12" lg="12" className="">
+                      <h5 className="description text-left">
+                      If we have piqued your interest and a position listed below appeals to you or if you have any queries please email us at manager@gpsonvermont.com.au.
+                      </h5>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col sm="12" lg="12" className="">
+                      <h5 className="description text-left">
+                      We look forward to hearing from you!
+                      </h5>
+                  </Col>
+              </Row>
+        </Container>
+      </ReactLinkify>  
+    </div>
 
         <div className="section text-center">
           <Container className=" text-muted">
@@ -46,32 +99,34 @@ const CareerPage = () => {
                       alt={job.title}
                     />
                     <section>
-                      <h3>{job.title}</h3>
+                      <ReactLinkify >
+                        <h3>{job.title}</h3>
 
-                      {job.descriptions.map((par, index) => (
-                        <p key={index}>{par}</p>
-                      ))}
-                      <div>
-                        {job.links.map((link, index) => (
-                          <cite key={index}>
-                            <a href={`tel: ${link.value}`}>{link.display}</a>
-                          </cite>
+                        {job.descriptions.map((par, index) => (
+                          <p key={index}>{par}</p>
                         ))}
-                      </div>
-                      
-                      <div className="p-1 text-center font-paragraph ">
-                      email us :
-                        <a
-                          className="text-info"
-                          href={`mailto: ${job.email}`}
-                        >
-                           {job.email}
-                        </a>
-                      </div>
-                      <footer>
-                        <br></br>
-                        <h5>{job.footer}</h5>
-                      </footer>
+                        <div>
+                          {job.links.map((link, index) => (
+                            <cite key={index}>
+                              <a href={`tel: ${link.value}`}>{link.display}</a>
+                            </cite>
+                          ))}
+                        </div>
+                        
+                        {/* <div className="p-1 text-center font-paragraph ">
+                        email us :
+                          <a
+                            className="text-info"
+                            href={`mailto: ${job.email}`}
+                          >
+                            {job.email}
+                          </a>
+                        </div> */}
+                        <footer>
+                          <br></br>
+                          <h5>{job.footer}</h5>
+                        </footer>
+                      </ReactLinkify>
                     </section>
                   </article>
                 </RibbonContainer>
